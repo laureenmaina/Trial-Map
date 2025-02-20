929504939530369d5a924d790e406d5afc506751
laureenmaina <mainalaureen58@gmail.com>
2025-02-20 14:52:12 +0300

    Loading

--- lib/google.dart
+++ lib/google.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-
 class MapsScreen extends StatefulWidget {
   const MapsScreen({super.key});
 
@@ -10,53 +9,53 @@ class MapsScreen extends StatefulWidget {
 }
 
 class _MapsScreenState extends State<MapsScreen> {
-  late GoogleMapController _mapController;
-  List<Marker> _markers = [];
-  bool showmaps = true ;
-
-@override
-  void initState(){
-    super.initState();
-    _markers.add(Marker(
+  GoogleMapController? _mapController;
+  final List<Marker> _markers = [
+    const Marker(
       markerId: MarkerId('mylocation'),
-      position: LatLng(36.8219, 1.2921),
-    ));
-        if (_markers.isNotEmpty){
-      setState(() {
-        showmaps = true ;
-      });
-  }
-    }
+      position: LatLng(-1.286389, 36.817223), // Nairobi, Kenya
+      infoWindow: InfoWindow(title: 'My Location'),
+    ),
+  ];
 
+  bool _isMapReady = false;
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      _mapController = controller;
+      _isMapReady = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.all(20),
-      child: showmaps ?  
-      Container
-      (
-        height: 300,
-        width: 1200,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
-        child: GoogleMap(
-          onMapCreated: (GoogleMapController controller) {
-            setState(() {
-              _mapController = controller;
-            });
-            },
-            markers: Set<Marker>.of(_markers),
-            mapType: MapType.terrain,
-            initialCameraPosition: CameraPosition(
-              target: LatLng(1.2921, 36.8219),
-              zoom: 13
+      child: _isMapReady
+          ? Container(
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.grey[200],
               ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: GoogleMap(
+                  onMapCreated: _onMapCreated,
+                  markers: Set<Marker>.of(_markers),
+                  mapType: MapType.terrain,
+                  initialCameraPosition: const CameraPosition(
+                    target: LatLng(-1.286389, 36.817223),
+                    zoom: 13,
+                  ),
+                ),
               ),
-        
-      
-    )
-    : CircularProgressIndicator(
-      color: Colors.white,
-    ));
+            )
+          : const Center(
+              child: CircularProgressIndicator(color: Colors.blue),
+            ),
+    );
   }
 }
+
